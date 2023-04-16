bf5c7086f28a4a201f2735d687c82e15af8f0941
Stephanie <stephanie.scm@gmail.com>
2023-04-16 11:05:55 -0500

    CALCULADORA API_16_04_23

--- index.js
+++ index.js
@@ -4,32 +4,42 @@ const app= express();
 
 app.use(express.json());
 
+app.use(function(req,res,next){
+       res.header( "Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Methods", "POST");
+       res.header("Access-Control-Allow-Headers", "Content-type");
+       next();
+
+
+}
+);
+
 app.post(
-    '/sumar', 
+    '/api/sumar', 
     (req,res)=>{
         const {numero_1,numero_2}=req.body;
-        const resultado=numero_1+numero_2;
+        const resultado=parseFloat(numero_1)+parseFloat(numero_2);
     res.json(resultado);
 
     }
 );
 app.post(
-    '/restar', 
+    '/api/restar', 
     (req,res)=>{
         const {numero_1,numero_2}=req.body;
-        const resultado=numero_1-numero_2;
+        const resultado=parseFloat(numero_1)-parseFloat(numero_2);
     res.json(resultado);
 
     }
 );
 
 app.post(
-    '/dividir', 
+    '/api/dividir', 
     (req,res)=>{
         let resultado;
         try{
         const {numero_1,numero_2}=req.body;
-        resultado=numero_1/numero_2;
+        resultado=parseFloat(numero_1)/parseFloat(numero_2);
     }catch(error){
         resultado=error;
     }
@@ -40,15 +50,15 @@ app.post(
 );
 
 app.post(
-    '/multiplicar', 
+    '/api/multiplicar', 
     (req,res)=>{
         const {numero_1,numero_2}=req.body;
-        const resultado=numero_1*numero_2;
+        const resultado=parseFloat(numero_1)*parseFloat(numero_2);
     res.json(resultado);
 
     }
 );
 app.listen(
-   3031, 
-   ()=>{console.log("el servidor esta ejecutandose en el puerto 3031");}
-);
+   3010, 
+   ()=>{console.log("el servidor esta ejecutandose en el puerto 3010");}
+); 
